233207368e060834c7170e5a09c20874afafc8ed
Oleksii Bobko <skellag@gmail.com>
2016-05-10 22:18:12 +0300

    store

--- src/main/java/com/pub/lookup/components/CachedSearchComponent.java
+++ src/main/java/com/pub/lookup/components/CachedSearchComponent.java
@@ -1,10 +1,7 @@
 package com.pub.lookup.components;
 
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
